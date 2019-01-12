fe00208e666901c078bad6d09de518c8c8a63198
belek <belektolubaev@gmail.com>
2019-01-12 12:00:12 +0600

    Добавление моделей и сохранение их в админку

--- exam/webapp/admin.py
+++ exam/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from webapp.models import UserInfo, Post
 
 # Register your models here.
+class UserInline(admin.StackedInline):
+    model = UserInfo
+    fields = ['phone', 'friend', 'photo']
+
+class UserInfoAdmin(UserAdmin):
+    inlines = [UserInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserInfoAdmin)
+admin.site.register(Post)
--- exam/webapp/models.py
+++ exam/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='info', verbose_name='Пользователи системы')
+    phone = models.CharField(max_length=20, null=True, blank=True, verbose_name='Телефон'),
+    friends = models.ForeignKey(User, related_name='friend', null=True, blank=True, verbose_name='Друзья', on_delete=models.PROTECT)
+    photo = models.ImageField(verbose_name='Фотография')
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Заголок")
+    text = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    author = models.ForeignKey(User, related_name='author', verbose_name='Автор поста', on_delete=models.PROTECT)
+
