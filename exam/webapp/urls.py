4913cf2642fac67ca63bdab6e0109d1815b45278
belek <belektolubaev@gmail.com>
2019-01-12 14:23:51 +0600

    Добавление шаблонов и урлов и вьюшек

---
+++ exam/webapp/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from webapp.views import PostListView, PostUpdateView, PostDetailView, PostCreateView, PostDeleteView, UserInfoListView, UserInfoDetailView, UserInfoUpdateView
+
+
+app_name = 'webapp'
+
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/create', PostCreateView.as_view(), name='post_create'),
+    path('order/<int:pk>/reject', PostDeleteView.as_view(), name='order_cancel'),
+    path('order/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
+    path('users', UserInfoListView.as_view(), name='food_list'),
+    path('users/<int:pk>', UserInfoDetailView.as_view(), name='food_detail'),
+    path('users/<int:pk>/update', UserInfoUpdateView.as_view(), name='food_update')
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
